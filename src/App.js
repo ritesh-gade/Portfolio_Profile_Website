b8fb0b42f14629adbc9c1e97a6a2aa5140602299
ritesh-gade <riteshgade16@gamil.com>
2024-03-07 01:52:28 +0530

    Added new

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
+      {/* particles */}
+
+      {/* navbar */}
+
+      {/* main page content */}
+      <Routes>
+        <Route index path='/' element={<Home/>} />
+        <Route path='/about' element={<About />} />
+        <Route path='/resume' element={<Resume />} />
+        <Route path='/skills' element={<Skills />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
 
     </div>
   );
